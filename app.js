814997e568c1c4eea80f7bc3d38d3bfeca4b1246
dhaka1436 <himanshudhaka987@gmail.com>
2024-04-07 15:57:53 +0530

    First Lecture done

---
+++ app.js
@@ -0,0 +1,53 @@
+
+
+const heading = React.createElement(
+    "h1",
+    {
+        id : "heading",
+        xyz : 'abc',
+    },
+    "Hello World from React using separete file"
+);
+
+console.log(heading); // will return an object to us
+        
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+console.log(typeof heading); // an object for us 
+
+
+
+
+/*
+    <div id = "parent">
+        <div id = "child1">
+            <h1> I am h1 tag </h1>
+            <h2> I am h2 tag </h2>
+        </div>
+        <div id = "child2">
+            <h1> I am h1 tag </h1>
+            <h2> I am h2 tag </h2>
+        </div> 
+    </div> 
+*/
+
+const parent = React.createElement(
+    "div",
+    {id : "parent"},
+    [
+        React.createElement("div",{id : "child1"},[
+            React.createElement("h1",{},"I am h1 tag"),
+            React.createElement("h2",{},"I am h2 tag")
+        ]),
+        React.createElement("div",{id : "child2"},[
+            React.createElement("h1",{},"I am h1 tag"),
+            React.createElement("h2",{},"I am h2 tag")
+        ])
+    ]
+
+); // nesting of the objects
+// JSX in the react
+
+
+console.log(parent);
+root.render(parent); // while rendering it converts object into html
