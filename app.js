48a87770c986097352299c35cd545dba013368c4
dhaka1436 <himanshudhaka987@gmail.com>
2024-04-09 23:03:22 +0530

    Second One Done

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import ReactDOM from "react-dom/client";
+import React from "react";
+
 
 
 const heading = React.createElement(
@@ -36,8 +39,8 @@ const parent = React.createElement(
     {id : "parent"},
     [
         React.createElement("div",{id : "child1"},[
-            React.createElement("h1",{},"I am h1 tag"),
-            React.createElement("h2",{},"I am h2 tag")
+            React.createElement("h1",{},"this is namaste react 😎  "),
+            React.createElement("h2",{},"I ANoushkas SIngh h2 tag")
         ]),
         React.createElement("div",{id : "child2"},[
             React.createElement("h1",{},"I am h1 tag"),
